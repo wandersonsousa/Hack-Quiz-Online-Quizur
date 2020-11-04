007ab4c178840e40233f72045a3da8c79cf6f6b1
W.Code <60992315+wandersonsousa@users.noreply.github.com>
2020-11-04 11:55:44 -0300

    Update bot_quiz.py

--- bot_quiz.py
+++ bot_quiz.py
@@ -64,62 +64,6 @@ def main():
     selecao_inteligente()
     sleep(10)
     driver.close()
-
-
-def selecao_inteligente():
-    sleep(5)
-    js_del_cookies = 'document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });'
-    driver.execute_script(js_del_cookies)
-    driver.refresh()
-    sleep(5)
-
-    print("iniciando...")
-    print(f"url: {url}")
-    print(f"username: {nome}")
-    ultima_questao_id.append(get_ls("alt_certa"))
-
-    alternativas_certas_ids.append(ultima_questao_id)
-    preencher_input_nome(nome)
-
-    _btn_iniciar = pegar_elemento("span.startBtn.a-dropTop.js-startQuiz")
-    _lista_sections = pegar_elementos("questionWrapper")
-
-    quantidade_de_questoes = len(_lista_sections)
-
-    sleep(2)
-    _btn_iniciar.click()
-    sleep(3)
-
-    print("iniciando selecao inteligente...")
-    for questao in range(quantidade_de_questoes):
-        for _section in _lista_sections:
-            sleep(0.2)
-            if _section.value_of_css_property("display") == "block":
-                _alternativa = pegar_elemento(
-                    f"li[data-alternative='{procurar_alternativa(pegar_id(_section))}']",
-                    _section,
-                )
-                _alternativa.click()
-                sleep(3)
-                break
-
-    print("Hacking terminado huashusah")
-    print("Fechando...")
-    driver.close()
-
-
-def get_ls(key):
-    return driver.execute_script(
-        "return window.localStorage.getItem(arguments[0]);", key
-    )
-
-
-def procurar_alternativa(section_id):
-    for id in alternativas_certas_ids:
-        if id[0] == section_id:
-            return id[1]
-
-
 def primeira_selecao():
     acessar_url(url)
     sleep(3)
@@ -182,10 +126,46 @@ def primeira_selecao():
                 break
 
 
-def pegar_id(_section):
-    id = _section.get_attribute("data-question")
-    return id
+def selecao_inteligente():
+    sleep(5)
+    js_del_cookies = 'document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });'
+    driver.execute_script(js_del_cookies)
+    driver.refresh()
+    sleep(5)
 
+    print("iniciando...")
+    print(f"url: {url}")
+    print(f"username: {nome}")
+    ultima_questao_id.append(get_ls("alt_certa"))
+
+    alternativas_certas_ids.append(ultima_questao_id)
+    preencher_input_nome(nome)
+
+    _btn_iniciar = pegar_elemento("span.startBtn.a-dropTop.js-startQuiz")
+    _lista_sections = pegar_elementos("questionWrapper")
+
+    quantidade_de_questoes = len(_lista_sections)
+
+    sleep(2)
+    _btn_iniciar.click()
+    sleep(3)
+
+    print("iniciando selecao inteligente...")
+    for questao in range(quantidade_de_questoes):
+        for _section in _lista_sections:
+            sleep(0.2)
+            if _section.value_of_css_property("display") == "block":
+                _alternativa = pegar_elemento(
+                    f"li[data-alternative='{procurar_alternativa(pegar_id(_section))}']",
+                    _section,
+                )
+                _alternativa.click()
+                sleep(3)
+                break
+
+    print("Hacking terminado huashusah")
+    print("Fechando...")
+    driver.close()
 
 def minerar_alternativas_certas(_lista_sections):
     print("minerando alternativas corretas")
@@ -196,29 +176,42 @@ def minerar_alternativas_certas(_lista_sections):
             _alternativa = _section.find_element_by_class_name("right")
             alternativa_id = _alternativa.get_attribute("data-alternative")
 
-            alternativas_certas_ids.append([section_id, alternativa_id])
+            alternativas_certas_ids.append([section_id, alternativa_id])    
+ def preencher_input_nome(nome):
+    nome_input = pegar_elemento("input.am_nameInput")
+    nome_input.clear()
+    nome_input.send_keys(nome)
+    nome_input.send_keys(Keys.RETURN)
+ def procurar_alternativa(section_id):
+    for id in alternativas_certas_ids:
+        if id[0] == section_id:
+            return id[1]
+    
+def get_ls(key):
+    return driver.execute_script(
+        "return window.localStorage.getItem(arguments[0]);", key
+    )
+
+
+        
+def pegar_id(_section):
+    id = _section.get_attribute("data-question")
+    return id
+
 
 
 def acessar_url(url):
     driver.get(url)
 
 
-def preencher_input_nome(nome):
-    nome_input = pegar_elemento("input.am_nameInput")
-    nome_input.clear()
-    nome_input.send_keys(nome)
-    nome_input.send_keys(Keys.RETURN)
-
 
 def pegar_elemento(path, container=driver):
     return container.find_element_by_css_selector(path)
 
-
 def pegar_elementos(className, container=driver):
     return container.find_elements_by_class_name(className)
 
-
+    
+    
+    
 main()
-
-for alt in alternativas_certas_ids:
-    print(f"section_id: {alt[0]} alternativa_id {alt[1]}")
